a6a32f6b6028afe2ece99e01981520ca995523bd
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-04-07 01:26:31 +0200

    mark as done

--- src/ContentToSpin.js
+++ src/ContentToSpin.js
@@ -1,15 +1,33 @@
 import React from 'react';
 
 function ContentToSpin(props) {
+  const token = props.token
   const history = props.history
   const video = history.location.state.video
   
   const finishEditingContent = (e) => {
     e.preventDefault()
-    console.log(video.id)
-    props.history.push({
-      pathname: '/dashboard/'
-    })
+
+    fetch(
+        process.env.REACT_APP_SPINNER_API_URL + '/done/' + video.id,
+        {
+          method: 'post',
+          headers: new Headers({
+            'Authorization': 'Bearer ' + token, 
+            'Content-Type': 'application/json'
+          }), 
+        }
+      ).then(response => {
+        if (response.status !== 204) {
+          return
+        }
+  
+        history.push({
+          pathname: '/dashboard/'
+        })
+      }).catch(error => {
+        // error
+      });
   }
 
   return (
