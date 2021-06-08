8db1fb1b662b8ec0c03c89c7c7864ff979e3d8f2
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-06-08 22:42:25 +0200

    todo comment to remember

--- src/Video/Clip/CropModalForm.tsx
+++ src/Video/Clip/CropModalForm.tsx
@@ -55,6 +55,8 @@ export default function CropModalForm(props: CropModalFormProps) {
 
         setError(null)
 
+        // TODO CHECK CLIO DEFAULT VALUE
+
         setValue(defaultValue)
 
     }, [clip, open])
