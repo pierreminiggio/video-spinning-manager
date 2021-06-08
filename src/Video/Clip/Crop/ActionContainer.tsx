e06d598acb2bfd0d72fa8fb02ea0e24b0c185cd8
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-06-08 22:39:58 +0200

    move action container

---
+++ src/Video/Clip/Crop/ActionContainer.tsx
@@ -0,0 +1,61 @@
+import {CSSProperties, MouseEvent, ReactElement} from "react";
+import {Button} from "@material-ui/core";
+import CropFunction from "../../../Struct/CropFunction";
+import {ReactComponent as Crop} from '../../../Resources/Svg/Crop.svg'
+import {ReactComponent as Trash} from '../../../Resources/Svg/Trash.svg'
+
+export default function ActionContainer(props: {
+    children: ReactElement,
+    index: number
+    indexes: number[]
+    onCrop: CropFunction
+    onDelete: CropFunction
+}) {
+    const {children, index, indexes, onCrop, onDelete} = props
+
+    const childrenLeft = children.props.style.left
+
+    const svgSize = 20
+    const padding = 10
+    const left = 'calc(' + childrenLeft + ' - ' + (svgSize / 2 + padding).toString() + 'px)'
+
+    const handleCropClick = (e: MouseEvent<HTMLButtonElement>) => {
+        preventDefault(e)
+        onCrop(index)
+    }
+
+    const handleDeleteClick = (e: MouseEvent<HTMLButtonElement>) => {
+        preventDefault(e)
+        onDelete(index)
+    }
+
+    const preventDefault = (e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        e.stopPropagation()
+    }
+
+    const buttonStyle: CSSProperties = {
+        padding: padding,
+        minWidth: 'auto',
+        display: 'block'
+    }
+
+    return <div
+        style={{position: 'absolute', left}}
+    >
+        <Button
+            style={buttonStyle}
+            onClick={handleCropClick}
+            onMouseDown={preventDefault}
+        >
+            <Crop fill="#3F51B5" width={svgSize} height={svgSize}/>
+        </Button>
+        {indexes.length > 1 ? <Button
+            style={buttonStyle}
+            onClick={handleDeleteClick}
+            onMouseDown={preventDefault}
+        >
+            <Trash fill="#FF0000" width={svgSize} height={svgSize}/>
+        </Button> : ''}
+    </div>
+}
--- src/Video/Clip/CropModalForm.tsx
+++ src/Video/Clip/CropModalForm.tsx
@@ -3,14 +3,13 @@ import { Alert } from "@material-ui/lab";
 import flexColumn from "../../Style/flexColumn";
 import gap from "../../Style/gap";
 import Clip from "../../Entity/Clip";
-import {ChangeEvent, CSSProperties, MouseEvent, ReactElement, useEffect, useState} from "react";
+import {ChangeEvent, MouseEvent, useEffect, useState} from "react";
 import ReactDOM from 'react-dom';
 import inputStep from "../../Domain/inputStep";
 import formatTime from "../../Formatter/formatTime";
-import {ReactComponent as Crop} from '../../Resources/Svg/Crop.svg'
-import {ReactComponent as Trash} from '../../Resources/Svg/Trash.svg'
 import NullableString from "../../Struct/NullableString";
 import CropFunction from "../../Struct/CropFunction";
+import ActionContainer from "./Crop/ActionContainer";
 
 const belowSliderContainerId = 'below-slider-container'
 const movementActionContainerPrefix = 'movement-actions-'
@@ -174,60 +173,4 @@ function ValueLabelComponent(props: ValueLabelProps) {
     >
         {children}
     </Tooltip>
-}
-
-function ActionContainer(props: {
-    children: ReactElement,
-    index: number
-    indexes: number[]
-    onCrop: CropFunction
-    onDelete: CropFunction
-}) {
-    const {children, index, indexes, onCrop, onDelete} = props
-
-    const childrenLeft = children.props.style.left
-
-    const svgSize = 20
-    const padding = 10
-    const left = 'calc(' + childrenLeft + ' - ' + (svgSize / 2 + padding).toString() + 'px)'
-
-    const handleCropClick = (e: MouseEvent<HTMLButtonElement>) => {
-        preventDefault(e)
-        onCrop(index)
-    }
-
-    const handleDeleteClick = (e: MouseEvent<HTMLButtonElement>) => {
-        preventDefault(e)
-        onDelete(index)
-    }
-
-    const preventDefault = (e: MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault()
-        e.stopPropagation()
-    }
-
-    const buttonStyle: CSSProperties = {
-        padding: padding,
-        minWidth: 'auto',
-        display: 'block'
-    }
-
-    return <div
-        style={{position: 'absolute', left}}
-    >
-        <Button
-            style={buttonStyle}
-            onClick={handleCropClick}
-            onMouseDown={preventDefault}
-        >
-            <Crop fill="#3F51B5" width={svgSize} height={svgSize}/>
-        </Button>
-        {indexes.length > 1 ? <Button
-            style={buttonStyle}
-            onClick={handleDeleteClick}
-            onMouseDown={preventDefault}
-        >
-            <Trash fill="#FF0000" width={svgSize} height={svgSize}/>
-        </Button> : ''}
-    </div>
 }
