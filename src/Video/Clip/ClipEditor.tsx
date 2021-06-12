1f19bde1bc4602459bada60d9c8dead3a1531f1d
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-06-12 21:38:12 +0200

    move code to ClipEditor

---
+++ src/Video/Clip/ClipEditor.tsx
@@ -0,0 +1,257 @@
+import Edit from "../Timeline/Edit";
+import Junk from "../Timeline/Junk";
+import Crop from "../Timeline/Crop";
+import {DragDropContext, DropResult} from "react-beautiful-dnd";
+import {Button} from "@material-ui/core";
+import Timeline from "../Timeline/Timeline";
+import {CSSProperties, useState} from "react";
+import Clip from "../../Entity/Clip";
+import ClipModalForm from "./ClipModalForm";
+import CropModalForm from "./CropModalForm";
+import flex from "../../Style/flex";
+import gap from "../../Style/gap";
+import VideoDuration from "../../Struct/VideoDuration";
+
+interface ClipEditorProps {
+    clips: Array<Clip>
+    setClips: any
+    contentId: number
+    orderedClips: Array<Clip>
+    finishedVideoHeight: number
+    finishedVideoWidth: number
+    totalClipTime: number
+    videoDuration: VideoDuration
+    videoWidth: number
+
+}
+
+export default function ClipEditor(props: ClipEditorProps) {
+    const {
+        clips,
+        setClips,
+        contentId,
+        orderedClips,
+        finishedVideoHeight,
+        finishedVideoWidth,
+        totalClipTime,
+        videoDuration,
+        videoWidth
+    } = props
+    const [dragging, setDragging] = useState(false)
+    const [selectedValue, setSelectedValue] = useState<Clip|Object>({})
+    const [formOpen, setFormOpen] = useState(false)
+    const [cropOpen, setCropOpen] = useState(false)
+
+    const handleFormClickOpen = () => {
+        openFormModal({})
+    };
+
+    const openFormModal = (value: Clip|Object) => {
+        setSelectedValue(value)
+        setFormOpen(true);
+    }
+
+    const openCropModal = (value: Clip) => {
+        setSelectedValue(value)
+        setCropOpen(true);
+    }
+
+    const handleFormClose = (clip: Object|Clip) => {
+        setFormOpen(false);
+
+        if (Object.keys(clip).length === 0) {
+            return
+        }
+
+        const newClipList: Array<Clip> = Array.from(clips)
+
+        // @ts-ignore
+        if (! clip.id) {
+            const newClipValues: Clip|Object = {...clip}
+            let maxId = 0
+            newClipList.forEach((newClip: Clip) => {
+                if (newClip.id > maxId) {
+                    maxId = newClip.id
+                }
+            })
+
+            // @ts-ignore
+            newClipValues.id = maxId + 1
+            // @ts-ignore
+            newClipValues.order = clips.length + 1
+            // @ts-ignore
+            newClipValues.moves = {}
+
+            // @ts-ignore
+            newClipList.push(newClipValues)
+        } else {
+            // @ts-ignore
+            const clipToEdit = newClipList.filter((newClip: Clip) => newClip.id === clip.id)[0]
+            // @ts-ignore
+            clipToEdit.start = clip.start
+            // @ts-ignore
+            clipToEdit.end = clip.end
+        }
+
+        setClips(newClipList);
+    };
+
+    const handleCropClose = (clip: Clip) => {
+        setCropOpen(false);
+        const newClipList: Array<Clip> = Array.from(clips)
+        const clipToEdit = newClipList.filter((newClip: Clip) => newClip.id === clip.id)[0]
+        clipToEdit.moves = clip.moves
+        setClips(newClipList);
+    };
+
+    const timelineId = 'timeline'
+    const junkId = 'junk'
+    const editId = 'edit'
+    const cropId = 'crop'
+
+    const onClipDragStart = (result: DropResult) => {
+        setDragging(true)
+    }
+
+    const onClipDragEnd = (result: DropResult) => {
+        setDragging(false)
+
+        const { destination, source, draggableId } = result
+
+        if (! destination) {
+            return
+        }
+
+        const sourceDroppableId = source.droppableId
+        const destinationDroppableId = destination.droppableId
+
+        const sourceIndex = source.index
+        const destinationIndex = destination.index
+
+        if (
+            sourceDroppableId === destinationDroppableId
+            && sourceIndex === destinationIndex
+        ) {
+            return
+        }
+
+        if (sourceDroppableId !== timelineId) {
+            return // I don't pull anything from edit or trash
+        }
+
+        let newClipList = Array.from(clips)
+        const draggedClipId = parseInt(draggableId)
+
+        if (destinationDroppableId === timelineId) {
+            const movedForward = destinationIndex - sourceIndex > 0
+            newClipList.forEach(newClip => {
+                if (newClip.id === draggedClipId) {
+                    newClip.order = destinationIndex
+                    return
+                }
+
+                if (
+                    movedForward
+                ) {
+                    if (newClip.order > sourceIndex && newClip.order <= destinationIndex) {
+                        newClip.order -= 1
+
+                        return
+                    }
+                }
+
+                if (newClip.order < sourceIndex && newClip.order >= destinationIndex) {
+                    newClip.order += 1
+
+                    return
+                }
+            })
+        } else if (destinationDroppableId === junkId) {
+            newClipList = newClipList.filter(newClip => newClip.id !== draggedClipId)
+            newClipList.forEach(newClip => {
+                if (newClip.order > sourceIndex) {
+                    newClip.order -= 1
+                }
+            })
+        } else if (destinationDroppableId === editId) {
+            const clipToEdit = newClipList.filter(newClip => newClip.id === draggedClipId)[0]
+            openFormModal(clipToEdit)
+            return
+        } else if (destinationDroppableId === cropId) {
+            const clipToCrop = newClipList.filter(newClip => newClip.id === draggedClipId)[0]
+            openCropModal(clipToCrop)
+            return
+        }
+
+        setClips(newClipList)
+    }
+
+    const appearingTransition = '.3s linear .3s'
+    const disappearingTransition = '.3s'
+    const appearingStyle: CSSProperties = {
+        opacity: dragging ? 1 : 0,
+        transition: dragging ? appearingTransition : disappearingTransition
+    }
+    const disappearingStyle: CSSProperties = {
+        opacity: dragging ? 0 : 1,
+        transition: dragging ? disappearingTransition : appearingTransition
+    }
+
+    // @ts-ignore
+    const clipToCrop: Clip = selectedValue
+
+    const buttonLineStyle: CSSProperties = {
+        ...flex,
+        justifyContent: 'center',
+        marginTop: gap / 2,
+        gap,
+        position: 'absolute',
+        width: '100%'
+    }
+
+    return <>
+        <ClipModalForm
+            selectedValue={selectedValue}
+            videoDuration={videoDuration}
+            open={formOpen}
+            onClose={handleFormClose}
+        />
+        <CropModalForm
+            clip={clipToCrop}
+            contentId={contentId}
+            finishedVideoHeight={finishedVideoHeight}
+            finishedVideoWidth={finishedVideoWidth}
+            open={cropOpen}
+            onClose={handleCropClose}
+        /><DragDropContext
+            onDragStart={onClipDragStart}
+            onDragUpdate={() => null}
+            onDragEnd={onClipDragEnd}
+        >
+            <div style={{position: 'relative', height: 80}}>
+                <div style={{...buttonLineStyle, ...disappearingStyle}}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleFormClickOpen}
+                        style={{zIndex: 1}}
+                    >
+                        Add a clip
+                    </Button>
+                </div>
+                <div style={{...buttonLineStyle, ...appearingStyle}}>
+                    <div><Edit editId={editId} /></div>
+                    <div><Crop cropId={cropId} /></div>
+                    <div><Junk junkId={junkId} /></div>
+                </div>
+            </div>
+            <Timeline
+                contentId={contentId ?? 0}
+                clips={orderedClips}
+                timelineId={timelineId}
+                totalTime={totalClipTime}
+                width={videoWidth}
+            />
+        </DragDropContext>
+    </>
+}
--- src/Video/Editor.tsx
+++ src/Video/Editor.tsx
@@ -1,21 +1,12 @@
-import { Button } from "@material-ui/core";
 import {CSSProperties, useEffect, useState} from "react";
-import {DragDropContext, DropResult} from "react-beautiful-dnd";
-import flex from "../Style/flex";
-import gap from "../Style/gap";
-import ClipModalForm from "./Clip/ClipModalForm";
 import RemotionPreview from "./Preview/RemotionPreview";
-import Edit from "./Timeline/Edit";
-import Junk from "./Timeline/Junk";
-import Timeline from "./Timeline/Timeline";
 import VideoDuration from "../Struct/VideoDuration";
 import VideoUrl from "../Struct/VideoUrl";
 import NullableNumber from "../Struct/NullableNumber";
 import Clip from "../Entity/Clip";
 import RemotionClip from "../Entity/Remotion/RemotionClip";
-import Crop from "./Timeline/Crop";
-import CropModalForm from "./Clip/CropModalForm";
 import {default as CropEntity} from '../Entity/Video/Clip/Crop/Crop'
+import ClipEditor from './Clip/ClipEditor'
 
 interface EditorProps {
     contentId: number
@@ -36,72 +27,6 @@ export default function Editor(props: EditorProps) {
         videoWidth
     } = props
     const [clips, setClips] = useState<Array<Clip>>([])
-    const [selectedValue, setSelectedValue] = useState<Clip|Object>({})
-    const [formOpen, setFormOpen] = useState(false)
-    const [cropOpen, setCropOpen] = useState(false)
-    const [dragging, setDragging] = useState(false)
-
-    const handleFormClickOpen = () => {
-        openFormModal({})
-    };
-
-    const openFormModal = (value: Clip|Object) => {
-        setSelectedValue(value)
-        setFormOpen(true);
-    }
-
-    const openCropModal = (value: Clip) => {
-        setSelectedValue(value)
-        setCropOpen(true);
-    }
-
-    const handleFormClose = (clip: Object|Clip) => {
-        setFormOpen(false);
-
-        if (Object.keys(clip).length === 0) {
-            return
-        }
-
-        const newClipList: Array<Clip> = Array.from(clips)
-
-        // @ts-ignore
-        if (! clip.id) {
-            const newClipValues: Clip|Object = {...clip}
-            let maxId = 0
-            newClipList.forEach((newClip: Clip) => {
-                if (newClip.id > maxId) {
-                    maxId = newClip.id
-                }
-            })
-
-            // @ts-ignore
-            newClipValues.id = maxId + 1
-            // @ts-ignore
-            newClipValues.order = clips.length + 1
-            // @ts-ignore
-            newClipValues.moves = {}
-
-            // @ts-ignore
-            newClipList.push(newClipValues)
-        } else {
-            // @ts-ignore
-            const clipToEdit = newClipList.filter((newClip: Clip) => newClip.id === clip.id)[0]
-            // @ts-ignore
-            clipToEdit.start = clip.start
-            // @ts-ignore
-            clipToEdit.end = clip.end
-        }
-
-        setClips(newClipList);
-    };
-
-    const handleCropClose = (clip: Clip) => {
-        setCropOpen(false);
-        const newClipList: Array<Clip> = Array.from(clips)
-        const clipToEdit = newClipList.filter((newClip: Clip) => newClip.id === clip.id)[0]
-        clipToEdit.moves = clip.moves
-        setClips(newClipList);
-    };
     
     const orderedClips = [...clips]
     orderedClips.sort((firstClip, secondClip) => {
@@ -114,88 +39,6 @@ export default function Editor(props: EditorProps) {
         
         return (secondClipOrder - firstClipOrder) > 0 ? -1 : 1
     })
-
-    const onClipDragStart = (result: DropResult) => {
-        setDragging(true)
-    }
-
-    const timelineId = 'timeline'
-    const junkId = 'junk'
-    const editId = 'edit'
-    const cropId = 'crop'
-    
-    const onClipDragEnd = (result: DropResult) => {
-        setDragging(false)
-
-        const { destination, source, draggableId } = result
-
-        if (! destination) {
-            return
-        }
-
-        const sourceDroppableId = source.droppableId
-        const destinationDroppableId = destination.droppableId
-
-        const sourceIndex = source.index
-        const destinationIndex = destination.index
-
-        if (
-            sourceDroppableId === destinationDroppableId
-            && sourceIndex === destinationIndex
-        ) {
-            return
-        }
-
-        if (sourceDroppableId !== timelineId) {
-            return // I don't pull anything from edit or trash
-        }
-
-        let newClipList = Array.from(clips)
-        const draggedClipId = parseInt(draggableId)
-
-        if (destinationDroppableId === timelineId) {
-            const movedForward = destinationIndex - sourceIndex > 0
-            newClipList.forEach(newClip => {
-                if (newClip.id === draggedClipId) {
-                    newClip.order = destinationIndex
-                    return
-                }
-
-                if (
-                    movedForward
-                ) {
-                    if (newClip.order > sourceIndex && newClip.order <= destinationIndex) {
-                        newClip.order -= 1
-
-                        return
-                    }
-                }
-
-                if (newClip.order < sourceIndex && newClip.order >= destinationIndex) {
-                    newClip.order += 1
-
-                    return
-                }
-            })
-        } else if (destinationDroppableId === junkId) {
-            newClipList = newClipList.filter(newClip => newClip.id !== draggedClipId)
-            newClipList.forEach(newClip => {
-                if (newClip.order > sourceIndex) {
-                    newClip.order -= 1
-                }
-            })
-        } else if (destinationDroppableId === editId) {
-            const clipToEdit = newClipList.filter(newClip => newClip.id === draggedClipId)[0]
-            openFormModal(clipToEdit)
-            return
-        } else if (destinationDroppableId === cropId) {
-            const clipToCrop = newClipList.filter(newClip => newClip.id === draggedClipId)[0]
-            openCropModal(clipToCrop)
-            return
-        }
-        
-        setClips(newClipList)
-    }
 
     let totalClipTime = 0
 
@@ -228,78 +71,21 @@ export default function Editor(props: EditorProps) {
     })
 
     const remotionProjectDurationInFrames = Math.ceil(totalClipTime * fps)
-
     const remotionProps = {props: JSON.stringify({clips: remotionClips})}
-
-    const appearingTransition = '.3s linear .3s'
-    const disappearingTransition = '.3s'
-    const appearingStyle: CSSProperties = {
-        opacity: dragging ? 1 : 0,
-        transition: dragging ? appearingTransition : disappearingTransition
-    }
-    const disappearingStyle: CSSProperties = {
-        opacity: dragging ? 0 : 1,
-        transition: dragging ? disappearingTransition : appearingTransition
-    }
-
-    // @ts-ignore
-    const clipToCrop: Clip = selectedValue
-
-    const buttonLineStyle: CSSProperties = {
-        ...flex,
-        justifyContent: 'center',
-        marginTop: gap / 2,
-        gap,
-        position: 'absolute',
-        width: '100%'
-    }
 
     return (
         <div>
-            <ClipModalForm
-                selectedValue={selectedValue}
-                videoDuration={videoDuration}
-                open={formOpen}
-                onClose={handleFormClose}
-            />
-            <CropModalForm
-                clip={clipToCrop}
+            <ClipEditor
+                clips={clips}
+                setClips={setClips}
                 contentId={contentId}
+                orderedClips={orderedClips}
                 finishedVideoHeight={finishedVideoHeight ?? 0}
                 finishedVideoWidth={finishedVideoWidth ?? 0}
-                open={cropOpen}
-                onClose={handleCropClose}
+                totalClipTime={totalClipTime}
+                videoDuration={videoDuration}
+                videoWidth={videoWidth}
             />
-            <DragDropContext
-                onDragStart={onClipDragStart}
-                onDragUpdate={() => null}
-                onDragEnd={onClipDragEnd}
-            >
-                <div style={{position: 'relative', height: 80}}>
-                    <div style={{...buttonLineStyle, ...disappearingStyle}}>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={handleFormClickOpen}
-                            style={{zIndex: 1}}
-                        >
-                            Add a clip
-                        </Button>
-                    </div>
-                    <div style={{...buttonLineStyle, ...appearingStyle}}>
-                        <div><Edit editId={editId} /></div>
-                        <div><Crop cropId={cropId} /></div>
-                        <div><Junk junkId={junkId} /></div>
-                    </div>
-                </div>
-                <Timeline
-                    contentId={contentId ?? 0}
-                    clips={orderedClips}
-                    timelineId={timelineId}
-                    totalTime={totalClipTime}
-                    width={videoWidth}
-                />
-            </DragDropContext>
             {totalClipTime > 0 ? <RemotionPreview
                 compositionHeight={finishedVideoHeight ?? 0}
                 compositionWidth={finishedVideoWidth ?? 0}
