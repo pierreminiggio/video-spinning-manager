83ef0a1b1b7a89290ddf3f1b4421d6176b911e33
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-06-17 22:56:06 +0200

    refacto junk

--- src/Video/Timeline/Junk.tsx
+++ src/Video/Timeline/Junk.tsx
@@ -1,29 +1,10 @@
-import styled from 'styled-components'
-import { Droppable } from 'react-beautiful-dnd'
 import trash from '../../Resources/Svg/Trash.svg'
-import DroppableButton from "./DroppableButton";
+import DroppableButtonContainer from "./DroppableButtonContainer";
 
-const Container = styled.div`
-    border: 1px solid red;
-    border-radius: 2px;
-    background-color: red;
-`
-
-interface JunkProps {
-    junkId: string
-}
-
-export default function Junk(props: JunkProps) {
-    const {junkId} = props
-    return <Container>
-        <Droppable droppableId={junkId} direction="horizontal">
-            {provided => <DroppableButton
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                backgroundImageUrl={trash}
-            >
-                {provided.placeholder}
-            </DroppableButton>}
-        </Droppable>
-    </Container>
+export default function Junk({junkId}: {junkId: string}) {
+    return <DroppableButtonContainer
+        backgroundColor={'red'}
+        backgroundImageUrl={trash}
+        droppableId={junkId}
+    />
 }
