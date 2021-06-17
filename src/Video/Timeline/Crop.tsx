090b643e46643dfdcfd17a660dde0a097879c371
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-06-17 23:01:20 +0200

    refacto crop

--- src/Video/Timeline/Crop.tsx
+++ src/Video/Timeline/Crop.tsx
@@ -1,42 +1,10 @@
-import styled from 'styled-components'
-import { Droppable } from 'react-beautiful-dnd'
 import crop from '../../Resources/Svg/Crop.svg'
+import DroppableButtonContainer from "./DroppableButtonContainer";
 
-const Container = styled.div`
-    border: 1px solid #3F51B5;
-    border-radius: 2px;
-    background-color: #3F51B5;
-`
-
-interface CropListProps {
-    crop: string
-}
-
-const CropList = styled.div`
-    padding: 8px;
-    display: flex;
-    background-image: url(${(props: CropListProps) => props.crop});
-    background-position: center;
-    background-repeat: no-repeat;
-    min-width: 34px;
-    min-height: 34px;
-`
-
-interface CropProps {
-    cropId: string
-}
-
-export default function Crop(props: CropProps) {
-    const {cropId} = props
-    return <Container>
-        <Droppable droppableId={cropId} direction="horizontal">
-            {provided => <CropList
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                crop={crop}
-            >
-                {provided.placeholder}
-            </CropList>}
-        </Droppable>
-    </Container>
+export default function Crop({cropId}: {cropId: string}) {
+    return <DroppableButtonContainer
+        backgroundColor={'#3F51B5'}
+        backgroundImageUrl={crop}
+        droppableId={cropId}
+    />
 }
