a4dae013e3918075a6e560100762a813fd653291
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-06-18 23:47:56 +0200

    move const out of containers

--- src/Video/Timeline/Action/DraggingActionsContainer.tsx
+++ src/Video/Timeline/Action/DraggingActionsContainer.tsx
@@ -2,6 +2,18 @@ import flex from "../../../Style/flex";
 import gap from "../../../Style/gap";
 import {CSSProperties} from "react";
 
+const buttonLineStyle: CSSProperties = {
+    ...flex,
+    justifyContent: 'center',
+    marginTop: gap / 2,
+    gap,
+    position: 'absolute',
+    width: '100%'
+}
+
+const appearingTransition = '.3s linear .3s'
+const disappearingTransition = '.3s'
+
 export default function DraggingActionsContainer({
     dragging,
     actions,
@@ -11,8 +23,6 @@ export default function DraggingActionsContainer({
     actions: JSX.Element
     draggingActions: JSX.Element
 }) {
-    const appearingTransition = '.3s linear .3s'
-    const disappearingTransition = '.3s'
     const appearingStyle: CSSProperties = {
         opacity: dragging ? 1 : 0,
         transition: dragging ? appearingTransition : disappearingTransition
@@ -20,15 +30,6 @@ export default function DraggingActionsContainer({
     const disappearingStyle: CSSProperties = {
         opacity: dragging ? 0 : 1,
         transition: dragging ? disappearingTransition : appearingTransition
-    }
-
-    const buttonLineStyle: CSSProperties = {
-        ...flex,
-        justifyContent: 'center',
-        marginTop: gap / 2,
-        gap,
-        position: 'absolute',
-        width: '100%'
     }
 
     return <div style={{position: 'relative', height: 80}}>
