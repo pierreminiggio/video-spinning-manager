5d41e5e7701e1846f8ef17b14409c2cc3e65243e
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2022-06-25 23:11:55 +0200

    dependency

--- src/Video/Subtitles/Languages.tsx
+++ src/Video/Subtitles/Languages.tsx
@@ -50,7 +50,7 @@ export default function Languages({languagesAndSubtitles, setLanguagesAndSubtitl
         }).catch(() => {
             setPullingSubtitles(false)
         });
-    }, [subtitlesUrl, token])
+    }, [subtitlesUrl, token, setLanguagesAndSubtitles])
 
     useEffect(() => {
 
