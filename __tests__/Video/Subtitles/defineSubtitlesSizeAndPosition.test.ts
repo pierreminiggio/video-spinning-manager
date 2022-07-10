119f5c602d69d5c3e99417bceebd5d1af9909b3a
Pierre Miniggio <pierre.miniggio@gmail.com>
2022-07-10 17:28:43 +0200

    more test size tests

--- __tests__/Video/Subtitles/defineSubtitlesSizeAndPosition.test.ts
+++ __tests__/Video/Subtitles/defineSubtitlesSizeAndPosition.test.ts
@@ -12,6 +12,16 @@ describe('Define Subtitles\' Size And Position', (): void => {
                 content: "J'sais pas si vous savez mais c'est incroyablement simplede faire ses propres yaourts.",
                 minWidth: 2.1,
                 maxWidth: 3.7
+            },
+            {
+                content: "Donc pour faire ça il faut un yaourt, et du lait, c'est tout !",
+                minWidth: 3,
+                maxWidth: 4.5
+            },
+            {
+                content: "*Bruits de yaourt*",
+                minWidth: 4,
+                maxWidth: 5.6
             }
         ]
         
